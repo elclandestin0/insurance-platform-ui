b0e78e4c32caa96c73d2318829ae614c5bb0f2fe
cherrypopsicle <majd.khoury93@gmail.com>
2023-12-26 17:43:18 +0000

    added policy creation

--- src/pages/policy-creators.tsx
+++ src/pages/policy-creators.tsx
@@ -1,13 +1,37 @@
 import React, {useEffect, useState} from 'react';
-import {Box, Flex, Text, SimpleGrid, Divider} from '@chakra-ui/react';
-import styles from "@/pages/page.module.css";
+import {
+    Box, Flex, Text, SimpleGrid, Divider, Button,
+    Modal, ModalOverlay, ModalContent, ModalHeader,
+    ModalCloseButton, ModalBody, ModalFooter, Input
+} from '@chakra-ui/react';import styles from "@/pages/page.module.css";
 import {policyMakerContract} from '@/utils/ethereum';
 import {useMetaMask} from "@/contexts/MetaMaskContext";
+import {ethers} from "ethers";
 
 const PolicyCreators: React.FC = () => {
     const {account} = useMetaMask();
     const [policies, setPolicies] = useState([]);
     const [error, setError] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [coverageAmount, setCoverageAmount] = useState('');
+    const [initialPremiumFee, setInitialPremiumFee] = useState('');
+    const [initialCoveragePercentage, setInitialCoveragePercentage] = useState('');
+    const [premiumRate, setPremiumRate] = useState('');
+    const [duration, setDuration] = useState('');
+    const [penaltyRate, setPenaltyRate] = useState('');
+    const [monthsGracePeriod, setMonthsGracePeriod] = useState('');
+
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
+    const fetchPolicies = async (): Promise<any[]> => {
+        try {
+            return await policyMakerContract.policies(account);
+        } catch (error) {
+            console.log(error);
+            return [];
+        }
+    }
 
     useEffect(() => {
         const getPolicies = async () => {
@@ -27,15 +51,37 @@ const PolicyCreators: React.FC = () => {
         return <Box>Error: {error}</Box>;
     }
 
-    const fetchPolicies = async (): Promise<any[]> => {
-        try {
-            return await policyMakerContract.policies(account);
-        } catch (error) {
-            console.log(error);
-            return [];
-        }
-    }
+    const handleSubmit = async (event: Event) => {
+        event.preventDefault();
+            try {
+                // Parse values to appropriate format
+                const parsedCoverageAmount = ethers.parseEther(coverageAmount); // Assuming ether units
+                const parsedInitialPremiumFee = ethers.parseEther(initialPremiumFee); // Adjust based on the unit
+                const parsedInitialCoveragePercentage = ethers.parseUnits(initialCoveragePercentage, 0);
+                const parsedPremiumRate = ethers.parseEther(premiumRate);
+                const parsedDuration = ethers.parseUnits(duration, 0);
+                const parsedPenaltyRate = ethers.parseUnits(penaltyRate, 0);
+                const parsedMonthsGracePeriod = ethers.parseUnits(monthsGracePeriod, 0);
+                
+                console.log(parsedCoverageAmount);
+                // Call the createPolicy function of the smart contract
+                const tx = await policyMakerContract.createPolicy(
+                    parsedCoverageAmount,
+                    parsedInitialPremiumFee,
+                    parsedInitialCoveragePercentage,
+                    parsedPremiumRate,
+                    parsedDuration,
+                    parsedPenaltyRate,
+                    parsedMonthsGracePeriod
+                );
+                await tx.wait();
 
+            } catch (error) {
+                console.error('Error creating policy:', error);
+            }
+        };
+
+    
     return (
         <Flex className={styles.main}
               height="100vh"
@@ -59,7 +105,36 @@ const PolicyCreators: React.FC = () => {
                 ) : (
                     <Text>No policies found.</Text>
                 )}
+                <Button colorScheme="blue" onClick={openModal}>Create new Policy</Button>
             </Box>
+            <Modal isOpen={isModalOpen} onClose={closeModal}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Create New Policy</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <form onSubmit={handleSubmit}>
+                            <Input placeholder="Coverage Amount" value={coverageAmount} onChange={(e) => setCoverageAmount(e.target.value)} />
+                            <Input placeholder="Initial Premium Fee" value={initialPremiumFee} onChange={(e) => setInitialPremiumFee(e.target.value)} />
+                            <Input placeholder="Initial Coverage Percentage" value={initialCoveragePercentage} onChange={(e) => setInitialCoveragePercentage(e.target.value)} />
+                            <Input placeholder="Premium Rate" value={premiumRate} onChange={(e) => setPremiumRate(e.target.value)} />
+                            <Input placeholder="Duration" value={duration} onChange={(e) => setDuration(e.target.value)} />
+                            <Input placeholder="Penalty Rate" value={penaltyRate} onChange={(e) => setPenaltyRate(e.target.value)} />
+                            <Input placeholder="Months Grace Period" value={monthsGracePeriod} onChange={(e) => setMonthsGracePeriod(e.target.value)} />
+                        </form>
+
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3} onClick={closeModal}>
+                            Close
+                        </Button>
+                        <Button variant="ghost" type="submit" form="form-id">
+                            Create Policy
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </Flex>
     );
 }
