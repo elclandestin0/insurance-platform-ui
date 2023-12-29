5397525340e7b55d54f3ca6e2c24d0d6cbbacb27
cherrypopsicle <majd.khoury93@gmail.com>
2023-12-29 17:51:57 +0000

    new font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,13 @@
 // src/pages/_app.tsx
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import '@/app/globals.css'; // Import global CSS
 import { MetaMaskProvider } from '../contexts/MetaMaskContext';
-import { extendTheme } from '@chakra-ui/react';
 import NavBar from "@/components/NavBar";
 
 const theme: Record<string, any> = extendTheme({
     fonts: {
-        heading: 'Roboto, sans-serif',
-        body: 'Poppins, sans-serif',
+        heading: '"Open Sans", sans-serif', // Use Open Sans for headings
+        body: '"Open Sans", sans-serif', // Use Open Sans for body text as well
     },
     // You can add other theme customizations here
 });
