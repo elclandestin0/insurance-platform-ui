f571ef6c1c624f38ad6ee00e3df6e7053bc21952
cherrypopsicle <majd.khoury93@gmail.com>
2024-01-09 16:07:45 +0000

    git add .

--- src/contracts/addresses.ts
+++ src/contracts/addresses.ts
@@ -1,3 +1,3 @@
-export const policyMakerAddress = "0x9b247A9d953774f8d70F37AE6B1338ee06F973Cf";
+export const policyMakerAddress = "0x39b1b01811Df4e76f8735F83c89184ebfb010c29";
 export const exploitationAddress = "0xE6056c9cB984eCd47b7ccCEd0eacfDd94A426F23";
 export const payoutAddress = "0x972356eE9e5bf89Bb747F4bBEE86CfbFBC6762EB";
